2e5248ece2b5d831e4af0c6f175adefe4ee53acf
jelelahmed <abduljelelahmed>
2024-01-04 14:26:31 +0100

    refactor the entry point of the app to start at index.tsx in the index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,44 @@
 import React, { FC } from 'react'
+
 import { Pane, majorScale } from 'evergreen-ui'
 import Container from '../components/container'
-import Hero from '../components/hero'
+import Intro from '../components/intro'
 import HomeNav from '../components/homeNav'
-import FeatureSection from '../components/featureSection'
 
-const Home: FC<{ content: { hero: any; features: any[] } }> = ({ content }) => {
+import fs from 'fs'
+import path from 'path'
+
+
+
+import { categories } from '../BLOG_DATA';
+import Directory from '../components/directory'
+
+const Home: FC<{ intro: any; content: any; categories: any }> = ({ intro, categories }) => {
+
+
+
   return (
-    <Pane>
-      <header>
-        <HomeNav />
-        <Container>
-          <Hero content={content.hero} />
-        </Container>
-      </header>
+    <Pane background="
+		#fdfdfd;
+			opacity: 0.8;
+			background-image:  radial-gradient(#141415 0.5px, transparent 0.5px), radial-gradient(#141415 0.5px, #fdfdfd 0.5px);
+			background-size: 20px 20px;
+			background-position: 0 0,10px 10px"
+		fontFamily="Source Sans Pro"
+		fontSize="16px"
+		fontWeight="lighter"
+		lineHeight="1.5"
+		letterSpacing="0.01"
+		>
+			<header>
+				<HomeNav />
+				<Container> 
+					<Intro content={intro} />
+				</Container>
+			</header>
       <main>
-        {content.features.map((feature, i) => (
-          <FeatureSection
-            key={feature.title}
-            title={feature.title}
-            body={feature.body}
-            image="/docs.png"
-            invert={i % 2 === 0}
-          />
-        ))}
+				<Directory categories={categories} />
       </main>
-      <footer>
-        <Pane background="overlay" paddingY={majorScale(9)}>
-          <Container>hello</Container>
-        </Pane>
-      </footer>
     </Pane>
   )
 }
@@ -38,11 +47,37 @@ const Home: FC<{ content: { hero: any; features: any[] } }> = ({ content }) => {
  * Should really get this content from our CMS
  */
 
+
 Home.defaultProps = {
-  content: {
-    features: [{ title: 'default feature', body: 'default body' }],
-    hero: { title: 'Jelels Hero', body: 'We are the best' },
-  },
+
+	intro: { title: "Blog Samples", body: "Welcome to a journey through words, where seeing truly is believing! I invite you to explore my writing samples, each a testament to my passion for crafting engaging and impactful content. Choose a category from the list below to immerse yourself in a collection of my previous works. Feel free to return to this page anytime; your literary adventure awaits. Enjoy the exploration!"},
+}
+
+
+
+
+export const getStaticProps = async () => {
+
+	const postsFolder = path.join(process.cwd(), 'contents');
+
+	const readFilenames = async () => {
+		try {
+			const filenames = await fs.promises.readdir(postsFolder);
+			console.log('Filenames:', filenames);
+			return filenames;
+		} catch (error) {
+			console.error('Error reading filenames:', error);
+			throw error;
+		}
+	};
+
+	const categories = await readFilenames()
+
+	return {
+		props: {
+		 categories,
+		},
+	}
 }
 
 export default Home

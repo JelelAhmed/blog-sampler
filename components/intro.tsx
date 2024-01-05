6a36c62de6ba5590ff844f386c80d0791f1e60c4
jelelahmed <abduljelelahmed>
2024-01-05 15:15:33 +0100

    updated package.json

--- components/intro.tsx
+++ components/intro.tsx
@@ -11,14 +11,14 @@ const Intro: FC<{ content: { title: string; body: string } }> = ({ content }) =>
       // height={`calc(100vh - ${majorScale(9)}px)`}
     >
       <Pane paddingX={majorScale(2)}>
-        <Heading fontSize="clamp(2rem, 8vw, 4.5rem)" lineHeight="clamp(2rem, 8vw, 6rem)" marginBottom={majorScale(1)}>
+        <Heading fontSize="clamp(2rem, 8vw, 4.5rem)" lineHeight="clamp(2rem, 8vw, 6rem)" marginBottom={majorScale(2)}>
           {content.title}
         </Heading>
         <Paragraph 
 					fontSize="clamp(1.2rem, 4vw, 1.2rem)" 
 					lineHeight="clamp(1.2rem, 4vw, 2rem)"
 					// fontFamily="Source Sans Pro"
-					fontWeight="lighter"
+					fontWeight="400"
 					letterSpacing="0.01"
 				>
           {content.body}
